06051370c9ef93ee8c20588ff8e7864a4f29b5c3
Zaine Umer <zu212942@reddies.hsu.edu>
2023-05-05 23:54:29 -0500

    trying to figure out the JS so that there is no errors in the console when changing pages

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-
+// Getting DOM Elements
 const mainSection = document.querySelector('.main-section');
 const mainHeading = document.querySelector('.main-heading');
-const mainParagraph = document.querySelector('.main-paragraph')
+const mainParagraph = document.querySelector('.main-paragraph');
 const footer = document.querySelector('.footer');
 
+const globalVariables = {
+    currentPage: typeof window !== 'undefined' ? window.location.pathname : null,
+  };
+
+  
+
+// on page load hide the paragraph and the header
+
 function onContentLoad () {
-    mainParagraph.style.display = 'none'
-    footer.style.display = 'none';
-    
+
+    if (globalVariables.currentPage === '/index.html') {
+        mainParagraph.style.display = 'none'
+        footer.style.display = 'none'; 
+    } 
 }
 
 function whenAnimationOver () {
-    if (mainParagraph.style.display === 'none' && footer.style.display ==='none') {
+    if (mainParagraph.style.display === 'none' && footer.style.display ==='none' && globalVariables.currentPage === '/index.html') {
+        mainParagraph.style.display = 'flex';
         mainParagraph.style.animation = 'fade-in 5s ease-in-out forwards';
-        footer.style.animation = 'fade-in 7s ease-in-out forwards';
-        mainParagraph.style.display = 'flex'
         footer.style.display = 'flex';
-    
+        footer.style.animation = 'fade-in 7s ease-in-out forwards';
+        mainSection.removeEventListener("animationend", whenAnimationOver);
     }
 }
 
-mainSection.addEventListener("animationend", whenAnimationOver);
-window.addEventListener("DOMContentLoaded", onContentLoad)
 
+
+function init() {
+    switch (globalVariables.currentPage) {
+      case "/":
+      case "/index.html":
+        console.log('Index');
+        break;
+      case "/about.html":
+        console.log('About');
+        break;
+      case "/videos.html":
+        console.log("Videos");
+        break;
+    }
+  }
+  
+  
+
+
+init ();
+mainSection.addEventListener("animationend", whenAnimationOver);
+window.addEventListener("DOMContentLoaded", onContentLoad);
