971340057a1caeb70a5d692102acfb851b9457d4
Zaine Umer <zaineumer@Zaines-MacBook-Pro.local>
2023-05-27 21:59:24 -0500

    Cleaning up the CSS

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
 // Getting DOM Elements
 const elements = {
-  mainSection: document.querySelector('.main-section'),
-  mainHeading: document.querySelector('.main-heading'),
-  mainParagraph: document.querySelector('.main-paragraph'),
-  footer: document.querySelector('.footer'),
-  hamburgerMenu: document.querySelector('#hamburger'),
-  currentYear: document.querySelector('#current-year'),
+  mainSection: document.querySelector(".main-section"),
+  mainHeading: document.querySelector(".main-heading"),
+  mainParagraph: document.querySelector(".main-paragraph"),
+  footer: document.querySelector(".footer"),
+  hamburgerMenu: document.querySelector("#hamburger"),
+  currentYear: document.querySelector("#current-year"),
 };
 
 const globalVariables = {
-  currentPage: typeof window !== 'undefined' ? window.location.pathname : null,
+  currentPage: typeof window !== "undefined" ? window.location.pathname : null,
 };
 
 // Helper function to update the current year
 function updateCurrentYear() {
   elements.currentYear.textContent = new Date().getFullYear();
-  console.log(elements.currentYear.textContent);
 }
 
 // Helper function to handle animation end event
 function handleAnimationEnd() {
-  if (elements.mainParagraph.style.display === 'none' && elements.footer.style.display === 'none') {
-    elements.mainParagraph.style.display = 'flex';
-    elements.mainParagraph.style.animation = 'fade-in 5s ease-in-out forwards';
-    elements.footer.style.display = 'flex';
-    elements.footer.style.animation = 'fade-in 7s ease-in-out forwards';
-    elements.mainSection.removeEventListener('animationend', handleAnimationEnd);
+  if (
+    elements.mainParagraph.style.display === "none" &&
+    elements.footer.style.display === "none"
+  ) {
+    elements.mainParagraph.style.display = "flex";
+    elements.mainParagraph.style.animation = "fade-in 5s ease-in-out forwards";
+    elements.footer.style.display = "flex";
+    elements.footer.style.animation = "fade-in 7s ease-in-out forwards";
+    elements.mainSection.removeEventListener(
+      "animationend",
+      handleAnimationEnd()
+    );
   }
 }
 
 // Function to initialize the page
 function initializePage() {
   switch (globalVariables.currentPage) {
-    case '/':
-    case '/index.html':
-      elements.mainParagraph.style.display = 'none';
-      elements.footer.style.display = 'none';
-      elements.mainSection.addEventListener('animationend', handleAnimationEnd);
-      console.log('Index');
+    case "/":
+    case "/index.html":
+      elements.mainParagraph.style.display = "none";
+      elements.footer.style.display = "none";
+      elements.mainSection.addEventListener("animationend", handleAnimationEnd);
+      console.log("Index");
       break;
-    case '/about.html':
-      console.log('About');
+    case "/about.html":
+      console.log("About");
       break;
-    case '/videos.html':
-      console.log('Videos');
+    case "/videos.html":
+      console.log("Videos");
       break;
   }
 }
